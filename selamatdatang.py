d624c5637da8b466643a3e57934f92bdb6e6bfa2
Edrian Hadinata <edrianhadinata@gmail.com>
2023-10-09 13:54:08 +0700

    first commit

---
+++ selamatdatang.py
@@ -0,0 +1,34 @@
+a = 1 #integer
+b = 2 #integer
+c = a + b
+
+print("NILAI c adalah : ",c)
+print()
+
+nama = "Edrian Hadinata" #string
+
+print("Nama saya ",nama)
+print()
+#namakamu = input("Siapa nama kamu ? :")
+#print("Selamat datang, ",namakamu)
+
+#double/float 3.14, 8.5
+#jarak dalam meter
+jarak = float(input("Jarak tempuh : "))
+print("Jarak kamu : ", jarak)
+print()
+#waktu dalam detik
+waktutmp = float(input("Waktu tempuh : "))
+print("Waktu tempuh : ", waktutmp)
+print()
+kec = jarak / waktutmp
+print("Kecepatan anda : ", kec, " m/s")
+
+
+
+
+
+
+
+
+
